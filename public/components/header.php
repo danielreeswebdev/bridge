ff1f9b3b7b1eb3d432d8ca07541a1df1fa63abfe
Daniel Rees <danielreeswebdev@gmail.com>
2022-01-11 15:04:24 +0000

    Home page structure built.

---
+++ public/components/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+	<title>Bridge</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="css/custom.css">
+</head>
+
+<body>
+	<div class="background-image"></div>
---
+++ public/index.php
@@ -0,0 +1,50 @@
+<?php include ('components/header.php'); ?>
+
+	<section id="calculating-total-costs" class="container-fluid d-flex h-100">
+		<div class="container justify-content-center align-self-center">
+			<div class="row">
+				<div class="col">
+					<h1 class="text-center">Calculating your total costs</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-8">
+					<button class="btn btn--white">Identify requirements <span><div class="estimate">Estimate</div>£6.00</span></button>
+				</div>
+				<div class="col-lg-8">
+					<button class="btn btn--white">Request a quotation <span><div class="estimate">Estimate</div>£3.00</span></button>
+				</div>
+				<div class="col-lg-8">
+					<button class="btn btn--white">Find products <span><div class="estimate">Estimate</div>£16.00</span></button>
+				</div>
+				<div class="col-lg-8">
+					<button class="btn btn--white">Raise an order <span><div class="estimate">Estimate</div>£6.00</span></button>
+				</div>
+				<div class="col-lg-8">
+					<button class="btn btn--white">Authorise sale <span><div class="estimate">Estimate</div>£21.55</span></button>
+				</div>
+				<div class="col-lg-8">
+					<button class="btn btn--white">Pay provider <span><div class="estimate">Estimate</div>£13.00</span></button>
+				</div>
+				<div class="col-lg-8">
+					<button class="btn btn--white">Deliver product <span><div class="estimate">Estimate</div>£4.30</span></button>
+				</div>
+				<div class="col-lg-8">
+					<button class="btn btn--white">Invoice check <span><div class="estimate">Estimate</div>£6.00</span></button>
+				</div>
+				<div class="col-lg-8">
+					<button class="btn btn--white">Place order <span><div class="estimate">Estimate</div>£6.50</span></button>
+				</div>
+			</div>
+			<div class="row">
+				<div class="offset-lg-8 col-lg-8">
+					<button class="btn btn--white btn--white--disabled" disabled>Total <span><div class="total">£0.00</button>
+				</div>
+				<div class="col-lg-8 text-center align-self-center">
+					<a href="/questions.php" class="btn btn-primary">Continue <span><img src="images/play.svg" alt="Play icon"></span></a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<?php include ('components/footer.php'); ?>
